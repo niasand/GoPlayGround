39a70d55034fed747448b7f0efecc088a8a0b9f0
Zhiwei Yang <tencrance@gmail.com>
2017-04-23 17:08:57 +0800

    ..

--- execrise-errors.go
+++ execrise-errors.go
@@ -6,16 +6,16 @@ import (
 
 type ErrNegativeSqrt float64
 
-func Sqrt(x float64) (float64 error) {
-	if x <= 0 {
-		return ErrNegativeSqrt(x)
-	}
-	return x
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %g", float64(e))
 
 }
 
-func (e ErrNegativeSqrt) Error() string {
-	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+func Sqrt(x float64) (float64 error) {
+	if x < 0 {
+		return ErrNegativeSqrt(x)
+	}
+	return nil
 
 }
 
