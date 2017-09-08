fa9d8d838487ac335bdacc458375a5e511440e04
Zhiwei Yang <tencrance@gmail.com>
2017-09-08 17:21:31 +0800

    ..

---
+++ helloworld-1.go
@@ -0,0 +1,13 @@
+/*
+* @Author: jerry
+* @Date:   2017-09-07 18:32:10
+* @Last Modified by:   jerry
+* @Last Modified time: 2017-09-07 18:32:18
+ */
+package main
+
+import "fmt" // Package implementing formatted I/O.
+
+func main() {
+	fmt.Printf("Καλημέρα κόσμε; or こんにちは 世界\n")
+}
