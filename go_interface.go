cd12f24fc52dfdc10753e1998120b9a150a1b864
Zhiwei Yang <tencrance@gmail.com>
2017-09-22 12:30:00 +0800

    go interface

---
+++ go_interface.go
@@ -0,0 +1,34 @@
+/*
+* @Author: jerry
+* @Date:   2017-09-22 12:26:09
+* @Last Modified by:   jerry
+* @Last Modified time: 2017-09-22 12:29:21
+ */
+package main
+
+import "fmt"
+
+type Phone interface {
+	call()
+}
+
+type NokiaPhone struct {
+}
+
+func (nokiaphone NokiaPhone) call() {
+	fmt.Println("I am nokia, I can call you")
+}
+
+type Iphone struct {
+}
+
+func (iphone Iphone) call() {
+	fmt.Println("I am iphone, I can call you")
+}
+
+func main() {
+	var phone Phone
+	phone = new(Iphone)
+	phone.call()
+
+}
