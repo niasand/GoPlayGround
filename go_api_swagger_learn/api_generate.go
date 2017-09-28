db9dc2941137b831e9db963186f6c0958016fe52
Zhiwei Yang <tencrance@gmail.com>
2017-09-28 19:24:19 +0800

    Knapsack_pack

---
+++ go_api_swagger_learn/api_generate.go
@@ -0,0 +1,14 @@
+/*
+* @Author: jerry
+* @Date:   2017-09-28 10:25:41
+* @Last Modified by:   jerry
+* @Last Modified time: 2017-09-28 10:26:22
+ */
+
+package main
+
+import "github.com/astaxie/beego"
+
+func main() {
+	beego.Run()
+}
